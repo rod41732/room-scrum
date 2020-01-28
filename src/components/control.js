50967b8b45426d53a3145bc1d2aa6b1feceb5a5c
Rodchananat <rod8711@gmail.com>
2020-01-28 23:36:02 +0700

    vote/reveal logic

--- src/components/control.js
+++ src/components/control.js
@@ -1,13 +1,13 @@
 import React, { useRef } from 'react';
 import "./control.scss";
 
-export const Control = ({onVote, onReveal}) => {
+export const Control = ({onVote, onReveal, hasReveal}) => {
   const voteRef = useRef();
   return <div className="control">
     <input className="input" ref={voteRef}></input>
     <button className="btn" onClick={() => {
       onVote(+voteRef.current.value);
     }}> Vote </button>
-    <button className="btn" onClick={onReveal}> Reveal </button>
+    <button className="btn" onClick={onReveal}> {hasReveal ? "Next" : "Reveal"} </button>
   </div>
 }
